a2d97f6fbe2180a22a4c66ff6d0641a1f5e89fa6
Norbert Schwarz <info@datentechnikschwarz.de>
2025-08-13 09:37:01 +0200

    Init

--- test/test.py
+++ test/test.py
@@ -13,16 +13,30 @@ def three_d_array(value, *dim):
     :return: 3D-array
     """
 
-    return [[[value for _ in range(dim[2])] for _ in range(dim[1])] for _ in range(dim[0])]
+    return [[[value for _ in range(dim[0])] for _ in range(dim[1])] for _ in range(dim[2])]
 
 if __name__ == "__main__":
-    array = three_d_array(False, *(2, 3, 1))
-    x = len(array)
+
+    default_value = 0
+    num_x = 3
+    num_y = 4
+    num_z = 2
+    
+    array = three_d_array(default_value, *(num_x, num_y, num_z))
+    
+    x = len(array[0][0])
     y = len(array[0])
-    z = len(array[0][0])
+    z = len(array)
+        
     print(x, y, z)
 
-    array[0][0][0] = True
-    array[1][1][0] = True
+    array[0][0][0] = 1
+    array[0][0][1] = 2
+    array[0][0][2] = 3
 
-    print(array)
+    array[0][1][0] = 4
+    array[0][1][1] = 5
+    array[0][1][2] = 6
+
+    for x in range(len(array)):
+        print(array[x])
