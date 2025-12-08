798e6f7ecb57b887b1f1a6c5b3e7e72c2d1bae5c
DTS4You <info@datentechnikschwarz.de>
2025-12-08 17:45:22 +0100

    Init

--- main.py
+++ main.py
@@ -13,6 +13,29 @@ time_on    = 0.3
 time_off   = 0.4
 time_pause = 1.5
 
+pix_array_01 = [11]
+pix_array_02 = [10]
+pix_array_03 = [ 6]
+pix_array_06 = [ 3, 4]
+pix_array_04 = [ 2 ,5]
+pix_array_05 = [ 0, 1]
+pix_array_07 = [ 7, 8]
+pix_array_08 = [ 9]
+pix_array_09 = [16, 24]
+pix_array_10 = [19]
+pix_array_11 = [17, 25]
+pix_array_12 = [29, 30]
+pix_array_13 = [28]
+pix_array_14 = [14, 22]
+pix_array_15 = [12, 13, 20, 21]
+pix_array_16 = [31, 32]
+
+obj_offset = 0          # Offset bei Zählung ab 1 = -1
+
+def blink_func():
+    MyWS2812.do_blink()
+ 
+
 # ------------------------------------------------------------------------------
 # --- Main Function                                                          ---
 # ------------------------------------------------------------------------------
@@ -28,117 +51,106 @@ def main():
     MyWS2812.do_all_def()
 
     try:
+        
         print("Start Main Loop")
  
-        while (True):
+        blink_couter = 0
+    
+        MyWS2812.do_all_def()	# Alle Leds auf Default-Wert
+       
+        # True
+        while MySerial.sercon_read_flag():
 
-            for i in range(0,2):                    # Laser
-                MyWS2812.set_led_obj(6,"on")
-                time.sleep(time_on)
-                MyWS2812.set_led_obj(6,"def")
-                time.sleep(time_off)
-            for i in range(0,2):                    # Boden 1
-                MyWS2812.set_led_obj(0,"on")
-                time.sleep(time_on)
-                MyWS2812.set_led_obj(0,"def")
-                time.sleep(time_off)
-            for i in range(0,2):                    # Spiegel / color_half
-                MyWS2812.set_led_obj(5,"on")
-                time.sleep(time_on)
-                MyWS2812.set_led_obj(5,"def")
-                time.sleep(time_off)
-            for i in range(0,2):                    # Empfänger
-                MyWS2812.set_led_obj(7,"on")
-                time.sleep(time_on)
-                MyWS2812.set_led_obj(7,"def")
-                time.sleep(time_off)
-            #----------------------------------------------------------
-            for i in range(0,2):                    # Laser
-                MyWS2812.set_led_obj(6,"on")
-                time.sleep(time_on)
-                MyWS2812.set_led_obj(6,"def")
-                time.sleep(time_off)
-            for i in range(0,2):                    # Boden 2
-                MyWS2812.set_led_obj(1,"on")
-                time.sleep(time_on)
-                MyWS2812.set_led_obj(1,"def")
-                time.sleep(time_off)
-            for i in range(0,2):                    # Spiegel
-                MyWS2812.set_led_obj(5,"on")
-                time.sleep(time_on)
-                MyWS2812.set_led_obj(5,"def")
-                time.sleep(time_off)
-            for i in range(0,2):                    # Empfänger
-                MyWS2812.set_led_obj(7,"on")
-                time.sleep(time_on)
-                MyWS2812.set_led_obj(7,"def")
-                time.sleep(time_off)
-            #----------------------------------------------------------
-            for i in range(0,2):                    # Laser
-                MyWS2812.set_led_obj(6,"on")
-                time.sleep(time_on)
-                MyWS2812.set_led_obj(6,"def")
-                time.sleep(time_off)
-            for i in range(0,2):                    # Boden 3
-                MyWS2812.set_led_obj(2,"on")
-                time.sleep(time_on)
-                MyWS2812.set_led_obj(2,"def")
-                time.sleep(time_off)
-            for i in range(0,2):                    # Spiegel
-                MyWS2812.set_led_obj(5,"on")
-                time.sleep(time_on)
-                MyWS2812.set_led_obj(5,"def")
-                time.sleep(time_off)
-            for i in range(0,2):                    # Empfänger
-                MyWS2812.set_led_obj(7,"on")
-                time.sleep(time_on)
-                MyWS2812.set_led_obj(7,"def")
-                time.sleep(time_off)
-            #----------------------------------------------------------
-            for i in range(0,2):                    # Laser
-                MyWS2812.set_led_obj(6,"on")
-                time.sleep(time_on)
-                MyWS2812.set_led_obj(6,"def")
-                time.sleep(time_off)
-            for i in range(0,2):                    # Boden 4
-                MyWS2812.set_led_obj(3,"on")
-                time.sleep(time_on)
-                MyWS2812.set_led_obj(3,"def")
-                time.sleep(time_off)
-            for i in range(0,2):                    # Spiegel
-                MyWS2812.set_led_obj(5,"on")
-                time.sleep(time_on)
-                MyWS2812.set_led_obj(5,"def")
-                time.sleep(time_off)
-            for i in range(0,2):                    # Empfänger
-                MyWS2812.set_led_obj(7,"on")
-                time.sleep(time_on)
-                MyWS2812.set_led_obj(7,"def")
-                time.sleep(time_off)
-            #----------------------------------------------------------
-            for i in range(0,2):                    # Laser
-                MyWS2812.set_led_obj(6,"on")
-                time.sleep(time_on)
-                MyWS2812.set_led_obj(6,"def")
-                time.sleep(time_off)
-            for i in range(0,2):                    # Boden 5
-                MyWS2812.set_led_obj(4,"on")
-                time.sleep(time_on)
-                MyWS2812.set_led_obj(4,"def")
-                time.sleep(time_off)
-            for i in range(0,2):                    # Spiegel
-                MyWS2812.set_led_obj(5,"on")
-                time.sleep(time_on)
-                MyWS2812.set_led_obj(5,"def")
-                time.sleep(time_off)
-            for i in range(0,2):                    # Empfänger
-                MyWS2812.set_led_obj(7,"on")
-                time.sleep(time_on)
-                MyWS2812.set_led_obj(7,"def")
-                time.sleep(time_off)
-            #----------------------------------------------------------            
+            if blink_couter > 50:
+                blink_couter = 0
+                blink_func()
+        
+            MySerial.sercon_read_line()
+            if MySerial.get_ready_flag():       # Zeichenkette empfangen
+                print(MySerial.get_string())
+                MyDecode.decode_input(str(MySerial.get_string()))
+                #MyDecode.decode_printout()
+                if MyDecode.get_valid_flag() == True:
+                    #print("Valid Command")
+                    if MyDecode.get_cmd_1() == "do":
+                        #print("do")
+                        if MyDecode.get_cmd_2() == "all":
+                            #print("all")
+                            if MyDecode.get_value_1() == 0:
+                                #print("off")
+                                MyWS2812.do_all_off()
+                            if MyDecode.get_value_1() == 1:
+                                #print("on")
+                                MyWS2812.do_all_on()
+                            if MyDecode.get_value_1() == 2:
+                                #print("def")
+                                MyWS2812.do_all_def()
+                        if MyDecode.get_cmd_2() == "obj":
+                            #print("obj")
+                            #print(MyDecode.get_value_1())
+                            #print(segment_map[MyDecode.get_value_1()])
+                            MyWS2812.do_all_off()
+                            if MyDecode.get_value_1() == 1:
+                                for i in pix_array_01:
+                                    MyWS2812.set_led_obj(i + obj_offset, MyDecode.get_value_2())
+                            if MyDecode.get_value_1() == 2:
+                                for i in pix_array_02:
+                                    MyWS2812.set_led_obj(i + obj_offset, MyDecode.get_value_2())
+                            if MyDecode.get_value_1() == 3:
+                                for i in pix_array_03:
+                                    MyWS2812.set_led_obj(i + obj_offset, MyDecode.get_value_2())
+                            if MyDecode.get_value_1() == 4:
+                                for i in pix_array_04:
+                                    MyWS2812.set_led_obj(i + obj_offset, MyDecode.get_value_2())
+                            if MyDecode.get_value_1() == 5:
+                                for i in pix_array_05:
+                                    MyWS2812.set_led_obj(i + obj_offset, MyDecode.get_value_2())
+                            if MyDecode.get_value_1() == 6:
+                                for i in pix_array_06:
+                                    MyWS2812.set_led_obj(i + obj_offset, MyDecode.get_value_2())
+                            if MyDecode.get_value_1() == 7:
+                                for i in pix_array_07:
+                                    MyWS2812.set_led_obj(i + obj_offset, MyDecode.get_value_2())
+                            if MyDecode.get_value_1() == 8:
+                                for i in pix_array_08:
+                                    MyWS2812.set_led_obj(i + obj_offset, MyDecode.get_value_2())
+                            if MyDecode.get_value_1() == 9:
+                                for i in pix_array_09:
+                                    MyWS2812.set_led_obj(i + obj_offset, MyDecode.get_value_2())
+                            if MyDecode.get_value_1() == 10:
+                                for i in pix_array_10:
+                                    MyWS2812.set_led_obj(i + obj_offset, MyDecode.get_value_2())
+                            if MyDecode.get_value_1() == 11:
+                                for i in pix_array_11:
+                                    MyWS2812.set_led_obj(i + obj_offset, MyDecode.get_value_2())
+                            if MyDecode.get_value_1() == 12:
+                                for i in pix_array_12:
+                                    MyWS2812.set_led_obj(i + obj_offset, MyDecode.get_value_2())
+                            if MyDecode.get_value_1() == 13:
+                                for i in pix_array_13:
+                                    MyWS2812.set_led_obj(i + obj_offset, MyDecode.get_value_2())
+                            if MyDecode.get_value_1() == 14:
+                                for i in pix_array_14:
+                                    MyWS2812.set_led_obj(i + obj_offset, MyDecode.get_value_2())
+                            if MyDecode.get_value_1() == 15:
+                                for i in pix_array_15:
+                                    MyWS2812.set_led_obj(i + obj_offset, MyDecode.get_value_2())
+                            if MyDecode.get_value_1() == 16:
+                                for i in pix_array_16:
+                                    MyWS2812.set_led_obj(i + obj_offset, MyDecode.get_value_2())
 
-            time.sleep(time_pause)
+                            #MyWS2812.set_led_obj(MyDecode.get_value_1(), MyDecode.get_value_2())
+
+                    if MyDecode.get_cmd_1() == "test":
+                        #print("Test")
+                        if MyDecode.get_cmd_2() == "led":
+                            #print("LED")
+                            MyWS2812.test_led(MyDecode.get_value_1(), MyDecode.get_value_2())
+                    
+
+            blink_couter = blink_couter + 1
+            # Loop-Delay !!!
+            time.sleep(0.01)        # 10ms
 
     except KeyboardInterrupt:
         print("Keyboard Interrupt")
